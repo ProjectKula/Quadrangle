08d1a434b41a52a0ec4b8ffd6acf9b5687595774
Shrish Deshpande <shrish.deshpande@icloud.com>
2023-12-12 22:48:34 +0530

    Simple login screen

---
+++ src/routes/login/+page.svelte
@@ -0,0 +1,56 @@
+<script lang="ts">
+	let id = "";
+	let password = "";
+	let idError: string | null = null;
+	let passwordError: string | null = null;
+
+	function validate(): boolean {
+		let valid: boolean = true;
+
+		if (!password) {
+			valid = false
+			passwordError = "Please enter a password"
+		}
+
+		if (!id) {
+			valid = false
+			idError = "Please enter USN / Email ID"
+		}
+
+		return valid;
+	}
+
+	function handleSubmit() {
+		if (validate()) {
+			console.log(`Login successful lol ${id} ${password}`)
+		}
+	}
+</script>
+
+<div class="flex items-center justify-center h-screen">
+	<div class="w-96 p-8 bg-white shadow-md rounded-md">
+		<h2 class="text-2xl font-semibold mb-4">Log In</h2>
+
+		<form on:submit|preventDefault={handleSubmit}>
+			<div class="mb-4">
+				<label for="usn" class="block text-sm font-medium text-gray-600">USN / Email ID</label>
+				<input type="text" id="usn" bind:value={id} on:input={() => idError = null} class="mt-1 p-2 w-full border rounded-md">
+				{#if idError}
+					<p class="text-red-500 text-xs mt-1">{idError}</p>
+				{/if}
+			</div>
+
+			<div class="mb-4">
+				<label for="password" class="block text-sm font-medium text-gray-600">Password</label>
+				<input type="password" id="password" bind:value={password} on:input={() => passwordError = null} class="mt-1 p-2 w-full border rounded-md">
+				{#if passwordError}
+					<p class="text-red-500 text-xs mt-1">{passwordError}</p>
+				{/if}
+			</div>
+
+			<button type="submit" class="w-full bg-blue-500 text-white p-2 rounded-md hover:bg-blue-600">
+				Log In
+			</button>
+		</form>
+	</div>
+</div>
