4adc88a801dd508e547be44b4be0628d68b52ad3
Shrish Deshpande <shrish.deshpande@icloud.com>
2024-01-04 13:38:29 +0530

    footer at bottom

--- src/lib/components/Sidebar.svelte
+++ src/lib/components/Sidebar.svelte
@@ -1,8 +1,6 @@
 <script>
 	import { faHome, faList, faUser } from '@fortawesome/free-solid-svg-icons';
 	import Fa from 'svelte-fa';
-	import { getContext } from 'svelte';
-	import { browser } from '$app/environment';
 
 	let menuItems = [
 		{ text: 'Home', icon: faHome, link: '/' },
@@ -14,12 +12,12 @@
 	let currentPath = '';
 </script>
 
-<div class="flex flex-col items-center p-4 bg-gray-800 text-white">
+<div class="flex flex-col items-center p-4 bg-blue-500 text-white">
 	{#each menuItems as { text, icon, link } (text)}
-		<a href={link} class:active={currentPath === link} class="{currentPath === link ? 'bg-blue-500' : ''} flex items-center p-2">
-			<div>
-				<i class="mr-2"><Fa icon={icon} /></i>
-				{text}
+		<a href={link} class:active={currentPath === link} class="flex items-center p-2">
+			<div class="flex flex-row items-center">
+				<i class="mr-2"><Fa size={"lg"} icon={icon} /></i>
+				<span>{text}</span>
 			</div>
 		</a>
 	{/each}
--- src/routes/(app)/+layout.svelte
+++ src/routes/(app)/+layout.svelte
@@ -5,17 +5,18 @@
 	import {faMagnifyingGlass} from '@fortawesome/free-solid-svg-icons'
 </script>
 
-<div class="min-h-screen flex">
-	<Sidebar />
-	<div class="flex-1 p-4">
-		<div class="mb-4 flex items-center">
-			<input type="text" placeholder="Search..." class="p-2 border rounded-md flex-1 mr-2">
-			<button class="p-2 bg-gray-800 text-white rounded-md"><Fa icon={faMagnifyingGlass} /></button>
-		</div>
-		<div class="flex-1">
-			<slot />
+<div class="min-h-screen flex flex-col">
+	<div class="flex flex-1">
+		<Sidebar />
+		<div class="flex-1 p-4">
+			<div class="mb-4 flex items-center">
+				<input type="text" placeholder="Search..." class="p-2 border rounded-md flex-1 mr-2">
+				<button class="p-2 bg-gray-800 text-white rounded-md"><Fa icon={faMagnifyingGlass} /></button>
+			</div>
+			<div class="flex-1">
+				<slot />
+			</div>
 		</div>
 	</div>
+	<Footer />
 </div>
-
-<Footer />
