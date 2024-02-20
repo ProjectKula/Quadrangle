b65749cfc8c1143885c7ddbe2554a464c0a1756d
Shrish Deshpande <shrish.deshpande@icloud.com>
2024-02-20 10:11:17 +0530

    kewl footer

--- src/lib/components/Footer.svelte
+++ src/lib/components/Footer.svelte
@@ -2,11 +2,11 @@
 	let copyrightText = '© 2023 cyberslothz and contributors.';
 </script>
 
-<div class="flex flex-col text-sm">
+<div class="flex flex-col text-sm text-neutral-500">
 	<h5 class="mb-4">{copyrightText}</h5>
-	<div class="flex flex-row">
-		<a href="/legal" class="mr-4 text-neutral-500">Legal</a>
+	<p>
+		<a href="/legal" class="mr-4">Legal</a>
 		•
 		<a href="https://github.com/cyberslothz" class="mr-4">Source</a>
-	</div>
+	</p>
 </div>
