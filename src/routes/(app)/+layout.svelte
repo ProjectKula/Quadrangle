7d98ddec7475e147e6b882f64b556cd1ff6d9cd7
Shrish Deshpande <shrish.deshpande@icloud.com>
2024-03-08 23:01:52 +0530

    Improve search bar ux

--- src/routes/(app)/+layout.svelte
+++ src/routes/(app)/+layout.svelte
@@ -1,8 +1,21 @@
-<script>
+<script lang="ts">
   import Sidebar from '$lib/components/Sidebar.svelte';
 	import Footer from '$lib/components/Footer.svelte';
 	import Fa from 'svelte-fa';
 	import {faMagnifyingGlass} from '@fortawesome/free-solid-svg-icons'
+	import { browser } from '$app/environment';
+
+	let searchBar: HTMLInputElement;
+
+	if (browser) {
+		// listen to '/' being pressed and focus searchBar
+		window.addEventListener('keydown', (e) => {
+			if (e.target?.tagName === 'BODY' && e.key === '/') {
+				e.preventDefault();
+				searchBar.focus();
+			}
+		});
+	}
 </script>
 
 <div class="flex flex-col min-h-screen">
@@ -11,10 +24,11 @@
 		<div class="flex-1">
 			<div class="mb-4 flex items-center border-b bg-neutral-200 border-neutral-300 dark:border-b-black dark:bg-neutral-800 p-2">
 				<input type="text"
+							 bind:this={searchBar}
 							 placeholder="Search..."
 							 class="bg-neutral-100 dark:bg-black text-lg p-2 border-good-grey rounded-md flex-1 mr-2"
 				>
-				<button class="p-2 bg-gray-200 dark:bg-gray-800 rounded-md"><Fa icon={faMagnifyingGlass} size="lg"/></button>
+				<button class="p-3 bg-neutral-500 hover:bg-neutral-600 transition rounded-md text-white"><Fa icon={faMagnifyingGlass} size="lg"/></button>
 			</div>
 			<div class="p-4">
 				<slot />
