13edc2edc1844b13a2e425f00e130a7224bd189d
Shrish Deshpande <shrish.deshpande@icloud.com>
2024-04-16 23:01:43 +0530

    bruh

--- src/lib/components/SignInWithGoogle.svelte
+++ src/lib/components/SignInWithGoogle.svelte
@@ -1,5 +1,7 @@
 <script>
   import { page } from '$app/stores';
+
+  console.log($page.url.origin)
 </script>
 
 <svelte:head>
@@ -7,15 +9,15 @@
 </svelte:head>
 
 <div id="g_id_onload"
-     data-client_id="1063344969029-el3e8icroegg8f7566okkvum7bgbrvb8.apps.googleusercontent.com"
-     data-context="signin"
-     data-ux_mode="redirect"
-     data-login_uri="{$page.url.origin}/googleCallback"
-     data-auto_prompt="false"
+  data-client_id="1063344969029-gk4b26m2av5q9qdsa4e3j566697hjf78.apps.googleusercontent.com"
+  data-context="signin"
+  data-ux_mode="redirect"
+  data-login_uri="http://localhost:5173/googleCallback"
+  data-auto_prompt="false"
 >
 </div>
 
-<div class="g_id_signin " 
+<div class="g_id_signin" 
      data-type="standard"
      data-shape="pill"
      data-text="signin_with"
