97d60f2cad9cbf3a4308809f856381c2caac9a8a
Shrish Deshpande <shrish.deshpande@icloud.com>
2024-06-19 23:31:22 +0530

    Less post width

--- src/routes/(app)/+page.svelte
+++ src/routes/(app)/+page.svelte
@@ -10,12 +10,12 @@
   <title>Home</title>
 </svelte:head>
 
-<div class="contents">
-  <div class="flex flex-row mb-2">
+<div class="flex flex-col w-full items-center">
+  <div class="flex flex-row mb-2 flex-1 w-full max-w-screen-sm">
     <NewPost />
   </div>
 
-  <div class="flex flex-col gap-4">
+  <div class="flex flex-col gap-4 flex-1 w-full max-w-screen-sm">
     {#each data.data as post}
       <RecentPostCard {post} />
     {/each}
