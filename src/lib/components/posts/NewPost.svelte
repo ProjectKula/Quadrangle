84a5de52e7c75e4948fbfac4eada870d877e3766
Shrish Deshpande <shrish.deshpande@icloud.com>
2024-06-20 14:07:25 +0530

    Allow using system file picker

--- src/lib/components/posts/NewPost.svelte
+++ src/lib/components/posts/NewPost.svelte
@@ -13,64 +13,73 @@
   let postText = '';
   let isSubmitVisible = false;
   let submitting = false;
-  $:isDragging = false;
+  $: isDragging = false;
   let textarea: HTMLTextAreaElement;
   let attachments: File[] = [];
-  
+
   function handleDragEnter(e) {
-    e.preventDefault()
+    e.preventDefault();
     if (isDragging) {
       return;
     }
-    console.log("start")
+    console.log('start');
     isDragging = true;
     textarea.classList.add('draggedTextArea');
     textarea.focus({ preventScroll: false });
   }
-  
+
   function handleDragLeave(e) {
-    console.log("end")
+    console.log('end');
     isDragging = false;
-    textarea.classList.remove('draggedTextArea')
+    textarea.classList.remove('draggedTextArea');
   }
-  
+
   let timeout: never;
-  let errorMessage: string = "";
-  
+  let errorMessage: string = '';
+
   function handleDrop(e) {
-    console.log("drop")
+    console.log('drop');
     isDragging = false;
-    
+
     if (e.dataTransfer.items[0]?.kind === 'file') {
       let item: DataTransferItem = e.dataTransfer.items[0];
-      let file: File = item.getAsFile()!;
-      let acceptedImageTypes: string[] = ['image/png', 'image/jpeg', 'image/gif', 'image/webp'];
-      if (acceptedImageTypes.includes(file.type)) {
-        if (file.size > 1024 * 1024 * 8) {
-          errorBanner("Maximum file size is 8MB");
-          return;
-        } else if (attachments.length >= 4) {
-          errorBanner("Maximum of 4 attachments");
-          return;
-        }
-        // postText = file.type + ' AND ' + file.name;
-        attachments.push(file);
-        attachments = attachments;
-        e.preventDefault();
-      } else {
-        errorBanner("Please upload an image");
+      e.preventDefault();
+      addFile(item.getAsFile()!, e);
+    }
+  }
+
+  let fileInput: HTMLInputElement;
+
+  function handleUpload() {
+    if (fileInput.files && fileInput.files[0]) {
+      addFile(fileInput.files[0]);
+    }
+  }
+
+  function addFile(file: File) {
+    let acceptedImageTypes: string[] = ['image/png', 'image/jpeg', 'image/gif', 'image/webp'];
+    if (acceptedImageTypes.includes(file.type)) {
+      if (file.size > 1024 * 1024 * 8) {
+        errorBanner('Maximum file size is 8MB');
+        return;
+      } else if (attachments.length >= 4) {
+        errorBanner('Maximum of 4 attachments');
+        return;
       }
+      // postText = file.type + ' AND ' + file.name;
+      attachments.push(file);
+      attachments = attachments;
+    } else {
+      errorBanner('Please upload an image');
     }
   }
 
   function errorBanner(message: string) {
     errorMessage = message;
     clearTimeout(timeout);
-    timeout = setTimeout(() => { errorMessage = ""; }, 2000);
-  }
-
-  function handleInputChange() {
-    isSubmitVisible = postText.length > 0;
+    timeout = setTimeout(() => {
+      errorMessage = '';
+    }, 2000);
   }
 
   function handleSubmit() {
@@ -81,7 +90,7 @@
     console.log('Submitting post:', newPostText);
     submitting = true;
     createNewPost(getAuthTokenClient(), newPostText, attachments)
-      .then(newPost => {
+      .then((newPost) => {
         if (newPost && newPost.id) {
           console.log('Created a new post:', newPost.id);
           setTimeout(() => {
@@ -91,29 +100,13 @@
           alert('Failed to create a new post');
         }
       })
-      .catch(err => {
+      .catch((err) => {
         alert('Error creating a new post');
         console.log(err);
       })
       .finally(() => {
         submitting = true;
       });
-  }
-  
-  function closeErrorBanner() {
-    errorMessage = "";
-  }
-  
-  function holdErrorBanner() {
-    if (timeout) {
-      clearTimeout(timeout);
-    }
-  }
-  
-  function restartErrorBanner() {
-    timeout = setTimeout(() => {
-      errorMessage = "";
-    }, 1000);
   }
 
   function deleteAttachment(index: number) {
@@ -124,37 +117,34 @@
 
 {#if errorMessage}
   <div transition:fade class="transition">
-    <ErrorBanner errorMessage={errorMessage} onClose={closeErrorBanner} onHover={holdErrorBanner} onUnhover={restartErrorBanner} />
+    <ErrorBanner
+      {errorMessage}
+      onClose={() => (errorMessage = '')}
+      onHover={() => {
+        if (timeout) clearTimeout(timeout);
+      }}
+      onUnhover={() =>
+        (timeout = setTimeout(() => {
+          errorMessage = '';
+        }, 1000))}
+    />
   </div>
 {/if}
 
 <div class="flex flex-col items-stretch w-full mx-auto mb-2 bg-neutral-100 dark:bg-neutral-800 rounded-xl border-0 p-2 border-neutral-500 gap-2">
-
   <div class="flex flex-row items-center p-1 gap-1">
     <span class="flex-1"></span>
     <button class="p-1 rounded-md transition hover:dark:bg-neutral-700 hover:bg-neutral-300"><BoldIcon /></button>
     <button class="p-1 rounded-md transition hover:dark:bg-neutral-700 hover:bg-neutral-300"><ItalicIcon /></button>
     <button class="p-1 rounded-md transition hover:dark:bg-neutral-700 hover:bg-neutral-300"><UnderlineIcon /></button>
     <span class="h-6 border-r mx-2"></span>
-    <button class="p-1 rounded-md transition hover:dark:bg-neutral-700 hover:bg-neutral-300"><PaperclipIcon /></button>
+    <label for="attachment-input">
+      <div role="button" class="p-1 rounded-md transition hover:dark:bg-neutral-700 hover:bg-neutral-300"><PaperclipIcon /></div>
+    </label>
+    <input bind:this={fileInput} type="file" id="attachment-input" class="hidden" on:change={handleUpload}/>
   </div>
-  
-  <textarea
-    bind:value={postText}
-    bind:this={textarea}
-    on:input={handleInputChange}
-    on:dragover={handleDragEnter}
-    on:drop={handleDrop}
-    on:dragleave={handleDragLeave}
-    placeholder={isDragging ? 'Drop your files here' : "Write something here..."}
-    disabled={submitting}
-    use:autosize
-    rows="4"
-    id="new-post-textarea"
-    class="w-full max-h-128 border-2 rounded-xl border-neutral-200 dark:border-good-dark-grey p-2 bg-neutral-100 dark:bg-neutral-800 text-base rounded resize-none"
-    class:bg-neutral-200={isDragging}
-    class:dark:bg-neutral-700={isDragging}
-  ></textarea>
+
+  <textarea bind:value={postText} bind:this={textarea} on:input={() => (isSubmitVisible = postText.length > 0)} on:dragover={handleDragEnter} on:drop={handleDrop} on:dragleave={handleDragLeave} placeholder={isDragging ? 'Drop your files here' : 'Write something here...'} disabled={submitting} use:autosize rows="4" id="new-post-textarea" class="w-full max-h-128 border-2 rounded-xl border-neutral-200 dark:border-good-dark-grey p-2 bg-neutral-100 dark:bg-neutral-800 text-base rounded resize-none" class:bg-neutral-200={isDragging} class:dark:bg-neutral-700={isDragging}></textarea>
 
   <div class="flex flex-row justify-end gap-2 transition fade-in-buttons">
     <button on:click={handleSubmit} class="btn-success px-8 py-1 text-white" disabled={!isSubmitVisible || submitting}>Submit</button>
