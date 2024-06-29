1118e44663d928b577e023ee1b5dca0f42309b81
Shrish Deshpande <shrish.deshpande@icloud.com>
2024-06-29 15:36:30 +0530

    Make post id link to the post's page

--- src/lib/components/posts/RecentPostCard.svelte
+++ src/lib/components/posts/RecentPostCard.svelte
@@ -10,9 +10,9 @@
   import type { Post } from '$lib/graphql/post';
 
   export let post: RecentPost | Post;
-  
+
   let likesCount = post.likesCount;
-  
+
   let selfLiked = post.selfLiked;
   let loading = false;
 
@@ -26,7 +26,7 @@
     hour: '2-digit',
     minute: '2-digit'
   });
-  
+
   function onLike() {
     loading = true;
     likePost(post.id, getAuthTokenClient())
@@ -38,7 +38,7 @@
         loading = false;
       });
   }
-  
+
   function onUnlike() {
     loading = true;
     unlikePost(post.id, getAuthTokenClient())
@@ -50,23 +50,23 @@
         loading = false;
       });
   }
+
 </script>
 
-<a href="/post/{post.id}" class="cursor-default flex flex-col flex-1 rounded-xl bg-neutral-100 dark:bg-neutral-800 p-2">
-
-  <div class="flex flex-row flex-shrink justify-between font-light gap-2 text-sm">
+<div class="cursor-auto flex flex-col flex-1 rounded-xl bg-neutral-100 dark:bg-neutral-800 p-2"s>
+  <div class="flex flex-row flex-shrink font-light gap-2 text-sm">
     <a class="text-lg dark:text-blue-400 dark:hover:text-blue-500 text-blue-500 hover:text-blue-600 transition" href="/user/{post.creator.id}">{post.creator.name}</a>
-    <span class="text-lg">{dateStr}, {timeStr}</span>
+    <span class="flex-1"></span>
+    <a href="/post/{post.id}/" class="text-lg">{dateStr}, {timeStr}</a>
   </div>
 
   <AttachmentCarousel attachments={post.attachments} />
-  
+
   <div class="min-width-0 text-lg">
     <SvelteMarkdown source={post.content} />
   </div>
 
   <p class="flex flex-row gap-4 pl-1">
-
     {#if selfLiked}
       <button class="text-red" on:click|preventDefault|stopPropagation={onUnlike}>
         <Fa icon={faHeartSolid} color="red" />
@@ -78,4 +78,4 @@
     {/if}
     {likesCount} Like{likesCount === 1 ? '' : 's'}
   </p>
-</a>
+</div>
